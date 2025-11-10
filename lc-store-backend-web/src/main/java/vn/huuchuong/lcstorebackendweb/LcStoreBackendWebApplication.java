ed7ac350439b83d064df2e1f6199cdcbd5fb5e19
huuchuong2004 <huuchuong398@gmail.com>
2025-11-10 11:59:57 +0700

    Init Project for project

---
+++ lc-store-backend-web/src/main/java/vn/huuchuong/lcstorebackendweb/LcStoreBackendWebApplication.java
@@ -0,0 +1,13 @@
+package vn.huuchuong.lcstorebackendweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcStoreBackendWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcStoreBackendWebApplication.class, args);
+    }
+
+}
---
+++ lc-store-backend-web/src/test/java/vn/huuchuong/lcstorebackendweb/LcStoreBackendWebApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.huuchuong.lcstorebackendweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcStoreBackendWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
